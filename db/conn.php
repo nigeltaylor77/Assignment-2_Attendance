fb50d3d40006d8a542cf0ec18544ebc112aed952
nigeltaylor77 <37161375+nigeltaylor77@users.noreply.github.com>
2021-12-30 15:10:22 -0500

    added remote  mysql connection

--- db/conn.php
+++ db/conn.php
@@ -1,9 +1,15 @@
 <?php 
 
-$host= 'localhost';
-$db='attendance_db';
-$user='root';
-$pass='';
+//$host= 'localhost';
+//$db='attendance_db';
+//$user='root';
+//$pass='';
+//$charset ='utf8mb4';
+
+$host= 'remotemysql.com';
+$db='CDJB4WhCqD';
+$user='CDJB4WhCqD';
+$pass='G6DWkcYfuo';
 $charset ='utf8mb4';
 
 $dsn= "mysql:host=$host;dbname=$db;charset=$charset";
